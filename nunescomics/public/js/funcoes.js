ead29997d50aba3954bdf60b43f0d681e3bdb5e5
Gabriel Frasson <bielfc27@gmail.com>
2021-05-20 15:45:54 -0300

    Listing categories page

--- nunescomics/public/js/funcoes.js
+++ nunescomics/public/js/funcoes.js
@@ -10,3 +10,42 @@ function addProduto(){
 function fecharJanela(el){
 	document.getElementById(el).style.visibility = "hidden";
 }
+
+function highlightButton(element) {
+	const pagination = document.querySelector('.pagination');
+	const activeButton = pagination.querySelector('.active');
+
+	if (activeButton) {
+		activeButton.classList.remove('active');
+	}
+
+	element.parentElement.classList.add('active');
+}
+
+function highlightNextButton() {
+	const pagination = document.querySelector('.pagination');
+	const pageItems = pagination.querySelectorAll('.page-item');
+	const pageItemsLength = pageItems.length;
+
+	for (let i = 1; i < pageItemsLength - 2; i++) {
+		if (pageItems[i].classList.contains('active')) {
+			pageItems[i].classList.remove('active');
+			pageItems[i + 1].classList.add('active');
+			break;
+		}
+	}
+}
+
+function highlightPreviousButton() {
+	const pagination = document.querySelector('.pagination');
+	const pageItems = pagination.querySelectorAll('.page-item');
+	const pageItemsLength = pageItems.length;
+
+	for (let i = pageItemsLength - 1; i > 1; i--) {
+		if (pageItems[i].classList.contains('active')) {
+			pageItems[i].classList.remove('active');
+			pageItems[i - 1].classList.add('active');
+			break;
+		}
+	}
+}
