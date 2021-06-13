f244c65a406a74b82473a315d072fcffadfe5c5e
Gabriel Frasson <bielfc27@gmail.com>
2021-06-13 20:46:34 -0300

    updated routes

--- app/routes.php
+++ app/routes.php
@@ -1,3 +1,25 @@
 <?php
+$router->get('index', 'PagesController@home');
+$router->get('about', 'PagesController@about');
+$router->get('contact', 'PagesController@contact');
+$router->get('login', 'PagesController@login');
+$router->get('items', 'PagesController@products');
+
+//routes adm user
+$router->get('admin/home', 'PagesController@homeAdm');
+$router->get('admin/users/list', 'UsersController@admUser');
+$router->post('users', 'UsersController@store');
+
+
+
+//routes adm category
+$router->get('admin/categories/list', 'CategoriesController@index');
+$router->get('admin/categories/add', 'CategoriesController@addCategory');
+$router->get('admin/categories/edit', 'CategoriesController@editCategory');
+$router->get('admin/categories/view', 'CategoriesController@viewCategory');
+
+$router->post('admin/categories/create', 'CategoriesController@create');
+$router->post('admin/categories/update', 'CategoriesController@edit');
+$router->post('admin/categories/delete', 'CategoriesController@delete');
 
 ?>
