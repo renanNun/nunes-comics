5abec178d1ec366422495bb15b4b55a19c9050eb
renanNun <renannunes400@gmail.com>
2021-06-13 13:20:55 -0300

    Corrigindo erro na organização das pastas

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 return [
     'database' => [
-        'name' => 'nunesStore',
+        'name' => 'nunes_store',
         'username' => 'root',
         'password' => '',
         'connection' => 'mysql:host=127.0.0.1',
